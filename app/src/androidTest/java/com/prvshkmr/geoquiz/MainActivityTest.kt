a356c7956baa27a3ac782f578cf304171fa0b23b
prvshkmr <parvesh427620@gmail.com>
2026-02-28 10:25:48 +0530

    MainActivity on Device Test

---
+++ app/src/androidTest/java/com/prvshkmr/geoquiz/MainActivityTest.kt
@@ -0,0 +1,36 @@
+package com.prvshkmr.geoquiz
+
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ActivityScenario.launch
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.experimental.theories.suppliers.TestedOn
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setUp() {
+        scenario = launch(MainActivity::class.java)
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+
+    @Test
+    fun showFirstQuestionOnLaunch(){
+        onView(withId(R.id.questionTextView))
+            .check(matches(withText(R.string.question_australia)))
+    }
+}
